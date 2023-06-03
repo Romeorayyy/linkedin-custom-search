7ddac01f2495e31b9757badb3077f62af3726855
Randy Yono <randyyono96@gmail.com>
2023-06-03 05:54:26 -0400

    added pagination

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [results, setResults] = useState([]);
   const [showResults, setShowResults] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   // New state variable for the current page number
   const [page, setPage] = useState(0);
@@ -15,19 +16,23 @@ function App() {
     searchGoogle();
   };
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    searchGoogle(newPage);
+  };
+
   // App.js
 
-  const searchGoogle = async () => {
+  const searchGoogle = async (page) => {
     try {
       const response = await axios.get('http://localhost:4000/api/search', {
         params: {
           query: searchQuery,
-          page, // Send the current page number to the API
+          page: page - 1, // Send the current page number (minus 1, as the library's first page is 0) to the API
         },
       });
       console.log(response.data.results);
-      // Append the new results to the existing ones
-      setResults(results.concat(response.data.results));
+      setResults(response.data.results);
       setShowResults(true);
     } catch (error) {
       console.error('Error:', error);
@@ -93,6 +98,16 @@ function App() {
             </div>
           ))}
           <button onClick={loadMoreResults}>Load More</button>
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <span>Page {currentPage}</span>
+          <button onClick={() => handlePageChange(currentPage + 1)}>
+            Next
+          </button>
         </div>
       )}
     </div>
